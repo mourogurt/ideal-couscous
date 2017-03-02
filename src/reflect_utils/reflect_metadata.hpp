342d68645b1b3b5f584be9ad4bccc10e1e3277aa
zatm8 <ya.maxis11@yandex.ru>
2017-03-02 12:05:15 +0300

    WIP Refactoring (Add some contexpr checks)

--- src/reflect_utils/reflect_metadata.hpp
+++ src/reflect_utils/reflect_metadata.hpp
@@ -13,23 +13,31 @@ using MetaClass = info::MetaClass<typename ::std::decay_t<typename T::MetaInfo_t
 
 template <class T>
 constexpr decltype (auto) get_class_name() {
+    if constexpr (!info::is_reflected_v<::std::decay_t<T>>) return ::boost::hana::nothing;
     return info::MetaClass<typename ::std::decay_t<typename T::MetaInfo_type> >::class_name;
 }
 
 template <class T, class Generator = info::DefaultIndexGenerator>
 constexpr decltype (auto) get_count() {
+    if constexpr (!info::is_reflected_v<::std::decay_t<T>>) return ::boost::hana::nothing;
+    if constexpr (!info::is_generator_v<::std::decay_t<Generator>>) return ::boost::hana::nothing;
     //Forcing unevaluated context to not interact with data(only with types)
     return boost::hana::size_c<decltype(::boost::hana::size(metautils::copy_tuple_sequence(MetaClass<T>::metadata,Generator::template generate<decltype(MetaClass<T>::metadata)>())))::value>;
 }
 
 template <class T, class Generator = info::DefaultIndexGenerator, class I>
 constexpr decltype (auto) get_name (I&& index) {
+    if constexpr (!info::is_reflected_v<::std::decay_t<T>>) return ::boost::hana::nothing;
+    if constexpr (!info::is_generator_v<::std::decay_t<Generator>>) return ::boost::hana::nothing;
+    if constexpr (decltype(::boost::hana::size(metautils::copy_tuple_sequence(MetaClass<T>::names,Generator::template generate<decltype(MetaClass<T>::metadata)>())))::value
+                  <= ::std::decay_t<I>::value) return ::boost::hana::nothing;
     return ::boost::hana::at(metautils::copy_tuple_sequence(MetaClass<T>::names,Generator::template generate<decltype(MetaClass<T>::metadata)>()),index);
 }
 
 template <class T, class Generator = info::DefaultIndexGenerator, class String>
 constexpr decltype (auto) find_name(String&& str) {
-    //constexpr auto tmp = metautils::copy_tuple_sequence(MetaClass<T>::names,Generator::template generate<decltype(MetaClass<T>::metadata)>());
+    if constexpr (!info::is_reflected_v<::std::decay_t<T>>) return ::boost::hana::nothing;
+    if constexpr (!info::is_generator_v<::std::decay_t<Generator>>) return ::boost::hana::nothing;
     return metautils::find_value_types(str,metautils::copy_tuple_sequence(MetaClass<T>::names,Generator::template generate<decltype(MetaClass<T>::metadata)>()));
 }
 
